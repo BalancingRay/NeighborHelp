309dc2b76a6ba07052149777b228bad9ec4517f4
Maxim Dudko <maxdudkas@gmail.com>
2021-04-30 23:50:34 +0300

    Add Order dublicate extension

--- NeighborHelp/Models/ModelsExtention.cs
+++ NeighborHelp/Models/ModelsExtention.cs
@@ -49,5 +49,47 @@ namespace NeighborHelp.Models
 
             return newUsers;
         }
+
+        public static Order Dublicate(this Order order)
+        {
+            var newOrder = new Order()
+            {
+                ID = order.ID,
+                Product = order.Product,
+                ProductDescription = order.ProductDescription,
+                Cost = order.Cost,
+                AuthorId = order.AuthorId,
+                OrderType = order.OrderType,
+                Status = order.Status
+            };
+
+            if (order.Author != null)
+            {
+                newOrder.Author = new UserProfile()
+                {
+                    Name = order.Author.Name,
+                    Address = order.Author.Address,
+                    Id = order.Author.Id,
+                    PhoneNumber = order.Author.PhoneNumber
+                };
+            }
+
+            return newOrder;
+        }
+
+        public static IList<Order> Dublicate(this IEnumerable<Order> orders)
+        {
+            var newOrders = new List<Order>();
+
+            if (orders == null || orders.Count() == 0)
+                return newOrders;
+
+            foreach (var item in orders)
+            {
+                newOrders.Add(Dublicate(item));
+            }
+
+            return newOrders;
+        }
     }
 }
