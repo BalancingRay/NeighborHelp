6e6745f5d84945eb16b3707dc0ce2f443aec3e71
Maxim Dudko <maxdudkas@gmail.com>
2021-05-03 00:22:29 +0300

    Add InMemoryDataBase option for SQL servers during Unit tests

--- NeighborHelpTests/Infrostructure/DataBaseBuilder.cs
+++ NeighborHelpTests/Infrostructure/DataBaseBuilder.cs
@@ -10,14 +10,24 @@ namespace NeighborHelpTests.Infrostructure
 {
     internal static class DataBaseBuilder
     {
-        private const string TEST_SERVER_SETTINGS = @"Server=(localdb)\mssqllocaldb;Database=TestDataBase;Trusted_Connection=True;";
+        private static bool useInMemoryImplementation = true;
+
+        private const string IN_MEMORY_SERVER_NAME = "NeighborHelpTestDataBaseInMemory";
+        private const string SQL_TEST_SERVER_SETTINGS = @"Server=(localdb)\mssqllocaldb;Database=TestDataBase;Trusted_Connection=True;";
 
         internal static ApplicationContext BuildDataBaseContext(bool clearOldData = true)
         {
 
             var options = new DbContextOptionsBuilder<ApplicationContext>()
-                    .UseSqlServer(TEST_SERVER_SETTINGS)
+                    .UseSqlServer(SQL_TEST_SERVER_SETTINGS)
                     .Options;
+
+            if (useInMemoryImplementation)
+            {
+                options = new DbContextOptionsBuilder<ApplicationContext>()
+                    .UseInMemoryDatabase(IN_MEMORY_SERVER_NAME)
+                    .Options;
+            }
 
             return new ApplicationContext(options, clearOldData);
         }
