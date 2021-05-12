6334d222a3735329abb0119d1db0d747298ee9d6
Maxim Dudko <maxdudkas@gmail.com>
2021-05-12 16:24:25 +0300

    Add API for chat hub

---
+++ NeighborHelpAPI/Consts/ChatHubConsts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeighborHelpAPI.Consts
+{
+    public static class ChatHubConsts
+    {
+        public const string Path = "/chat";
+
+        //From server to Clients
+        public const string NotifyClients = "Notify";
+        public const string ReceiveClientsMesage = "Receive";
+
+        //From clients to server
+        public const string SendMessage = "Send";
+        public const string SendToGroup = "SendToGroup";
+        public const string EnderToGroup = "Enter";
+    }
+}
