ac9eb4680ff7d84027ad52db06c9906bd8aed802
Maxim Dudko <maxdudkas@gmail.com>
2021-05-14 17:19:32 +0300

    Add extention to setting static files middlewares. Set coping static files properties in csproj

---
+++ NeighborHelpWebClient/Utils/StartupStaticFilesExtention.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.StaticFiles.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileProviders;
+using NeighborHelpInfrastucture.Utils;
+
+namespace NeighborHelpWebClient.Utils
+{
+    public static class StartupStaticFilesExtention
+    {
+        public const string UseStaticFilesPropertyName = "UseStaticFiles";
+
+        public static IApplicationBuilder UseWebClientStaticFiles(this IApplicationBuilder app, IConfiguration DirectoryConfiguration, IWebHostEnvironment env)
+        {
+            bool useStatic = DirectoryConfiguration.ReadBoolProperty(UseStaticFilesPropertyName);
+
+            if (useStatic)
+            {
+                var path = env.WebRootPath;
+
+                if (DirectoryConfiguration["ASPNETCORE_ENVIRONMENT"] == "Development")
+                {
+                    path = GetRedirectedWebPath(path);
+                    env.WebRootPath = path;
+                }
+
+                var options = new SharedOptions()
+                {
+                    FileProvider = new PhysicalFileProvider(path)
+                };
+                var fileOptions = new DefaultFilesOptions(options);
+                fileOptions.DefaultFileNames.Add("Login.html");
+                fileOptions.DefaultFileNames.Add("Chat.html");
+
+                app.UseDefaultFiles(fileOptions);
+                app.UseStaticFiles(new StaticFileOptions(options));
+            }
+            return app;
+        }
+
+        private static string GetRedirectedWebPath(string currentPath)
+        {
+            var newPath = currentPath.Replace("\\NeighborHelp\\NeighborHelp\\wwwroot", "\\NeighborHelp\\NeighborHelpWebClient\\wwwroot");
+            return newPath;
+        }
+    }
+}
